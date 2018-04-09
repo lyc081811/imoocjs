1d2e2e6cfc58722d7f1f0bc45170cb655eae546c
lyc <269632358@qq.com>
2018-04-09 19:40:28 +0800

    修订一下

--- express-demo/routes/index.js
+++ express-demo/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
+// router.use(function timeLog(req, res, next) {
+//   console.log('Time: ', Date.now());
+//   next();
+// });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '重新拾起得node' });
 });
 router.get('/png', function(req, res, next) {
   res.download('public/img/1.png');
