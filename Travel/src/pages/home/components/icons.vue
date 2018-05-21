c300c12bdf3970a72ca2424664cb3bdadef80c92
lyc081811 <269632358@qq.com>
2018-05-22 00:19:59 +0800

    32

--- Travel/src/pages/home/components/icons.vue
+++ Travel/src/pages/home/components/icons.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="icons">
     <swiper :options="swiperOption">
-      <swiper-slide >
-        <div class="icon" v-for="item of swiperIconsList" :key="item.id">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img"><img :src="item.url" alt=""></div>
           <p class="icon-desc">{{item.txt}}</p>
         </div>
@@ -88,31 +88,35 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl';
+@import '~styles/varibles.styl'
+@import '~styles/mixin.styl'
 .icons >>> .swiper-container
   height 0
   padding-bottom 50%
-.icon
-  position relative
-  float left
-  overflow hidden
-  width 25%
-  height 0
-  padding-bottom 25%
-  .icon-img
-    position absolute
-    width 100%
-    bottom .44rem
-    top 0
-    text-align center
-    img
-      height 100%
-  .icon-desc
-    color $darkTextColor
-    text-align center
-    position absolute
-    width 100%
-    height .44rem
-    line-height .44rem
-    bottom 0
+.icons
+  margin-top .1rem
+  .icon
+    position relative
+    float left
+    overflow hidden
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      width 100%
+      bottom .44rem
+      top 0
+      text-align center
+      img
+        height 100%
+    .icon-desc
+      color $darkTextColor
+      text-align center
+      position absolute
+      width 100%
+      height .44rem
+      line-height .44rem
+      bottom 0
+      ellipsis()
 </style>
---
+++ Travel/src/pages/home/components/recommend.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <img class="item-img" src="http://img1.qunarzz.com/sight/201405/27/4e1c7943ceb94759c8d65eac.jpg_150x150_1b5d04b2.jpg" alt="">
+        <div class="item-info">
+          <p class="item-title">青岛东方影都万达乐园</p>
+          <p class="item-desc">10条评论</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+<style lang="stylus" scoped>
+.title
+  margin-top .2rem
+  line-height .8rem
+  background #eee
+  text-indent .2rem
+.item
+  display flex
+  height 1.9rem
+  overflow hidden
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding .1rem
+  .item-info
+    flex 1
+
+</style>
