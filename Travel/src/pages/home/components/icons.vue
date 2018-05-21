4faf9eb64df9035613dd5a16d01a8c65d46acfa9
lyc <269632358@qq.com>
2018-05-21 17:20:51 +0800

    提交了

---
+++ Travel/src/pages/home/components/icons.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <div class="icon">
+          <div class="icon-img"><img :src="item.url" alt=""></div>
+          <p class="icon-desc">{{item.txt}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Icons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [
+        {
+          id: '01',
+          url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          txt: '景点门票'
+        },
+        {
+          id: '02',
+          url: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          txt: '必游榜单'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.icons >>> .swiper-container
+  height 0
+  padding-bottom 50%
+.icon
+  position relative
+  float left
+  overflow hidden
+  width 25%
+  height 0
+  padding-bottom 25%
+  .icon-img
+    position absolute
+    width 100%
+    bottom .44rem
+    top 0
+    text-align center
+    img
+      height 100%
+  .icon-desc
+    color $darkTextColor
+    text-align center
+    position absolute
+    width 100%
+    height .44rem
+    line-height .44rem
+    bottom 0
+</style>
---
+++ Travel/src/pages/home/components/swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '01',
+          url: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+        },
+        {
+          id: '02',
+          url: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff!important
+.wrapper
+  width 100%
+  height 0
+  overflow hidden
+  padding-bottom 26.67%
+  .swiper-img
+    width 100%
+</style>
