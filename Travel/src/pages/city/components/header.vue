b0c5fb2e81712cf3e44c35c56fa5abf27fbc0a3a
lyc081811 <269632358@qq.com>
2018-05-28 23:44:58 +0800

    er

---
+++ Travel/src/pages/city/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="header">
+		城市选择
+	</div>
+</template>
+<script>
+export default {
+  name: "CityHeader"
+};
+</script>
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.header {
+		height: 0.86rem;
+		line-height: 0.86rem;
+		text-align: center;
+		color: #fff;
+		background $bgcolor
+		font-size .32rem
+	}
+</style>
