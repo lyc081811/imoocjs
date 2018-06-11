0c31e6da8f4be6502c479cf597cc7003c7956ecb
lyc <269632358@qq.com>
2018-06-11 17:37:19 +0800

    添加music

---
+++ vue-music/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello world
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+@import "common/stylus/variable"
+#app
+  color $color-theme
+</style>
