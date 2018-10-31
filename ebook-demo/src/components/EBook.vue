8585d3da9e1dc47d305e2afd05789c0aedb29f2e
lyc <269632358@qq.com>
2018-10-31 17:37:49 +0800

    添加ebook代码

---
+++ ebook-demo/src/components/EBook.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+aaaa
+  </div>
+</template>
+<script>
+import Epub from 'epubjs'
+const DOWN_URL = '/static/1.epub'
+export default {
+  methods: {
+    // 电子书的渲染
+    showEpub () {
+      // 生成book
+      this.book = new Epub(DOWN_URL)
+      console.log(this.book)
+      // 生成rednition
+      // 通过rendtion.display渲染电子书
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+@import '~@/assets/style/global'
+</style>
---
+++ ebook-demo/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import EBook from '@/components/EBook'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/ebook'
+    },
+    {
+      path: '/ebook',
+      name: 'EBook',
+      component: EBook
+    }
+  ]
+})
